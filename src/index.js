885b1f828c8e8dc958b14c73dd0a33df3ed10d60
yvesaur <yveschristianpcasio@gmail.com>
2025-10-02 02:22:43 +0800

    feat:

--- src/index.js
+++ src/index.js
@@ -3,12 +3,47 @@
 import "./assets/styles/normalize.css";
 /* ========================================================================== */
 
+import "./assets/styles/colorPalette.css";
+import "./index.css";
+
 /* Custom Styles
    ========================================================================== */
 import "./assets/styles/components/buttonStyles.css";
 import "./assets/styles/layoutStyles.css";
 /* ========================================================================== */
-import "./assets/styles/colorPalette.css";
-import "./index.css";
+
+/* Pages import
+   ========================================================================== */
+import generateHomepage from "./pages/home/home";
+/* ========================================================================== */
+
+const mainElement = document.querySelector("body > main");
+const navButtons = document.querySelectorAll(".hero-nav > a");
+
+console.log(navButtons);
+
+function initHomepage() {
+	mainElement.innerHTML = "";
+	mainElement.innerHTML += generateHomepage();
+}
+
+function addMenuPage() {
+	mainElement.innerHTML = "";
+}
+
+function addContactPage() {
+	mainElement.innerHTML = "";
+}
+
+// Home Nav button
+navButtons[0].addEventListener("click", initHomepage);
+
+// Menu Nav button
+navButtons[1].addEventListener("click", addMenuPage);
+
+// Contact Nav button
+navButtons[2].addEventListener("click", addContactPage);
+
+initHomepage();
 
 console.log("Webpack is running");
---
+++ src/pages/home/home.js
@@ -0,0 +1,65 @@
+import "./home.css";
+
+function generateHomepage() {
+	return `
+      <div class="home-introduction">
+				<p>
+					Welcome to Dewy Berry, where the rich flavors of Italy come alive in
+					every dish. From handcrafted pastas to wood-fired pizzas, we serve
+					fresh, authentic meals made to bring people together.
+				</p>
+				<p>
+					At Dewy Berry, we bring you the warmth and flavor of authentic Italian
+					cuisine in a cozy, welcoming atmosphere. Join us on weekdays from 11
+					AM to 10 PM and savor every moment with us.
+				</p>
+			</div>
+			<table class="operating-hours-table">
+				<thead>
+					<tr>
+						<th colspan="2">Operating Hours</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Monday</td>
+						<td>11am - 10pm</td>
+					</tr>
+					<tr>
+						<td>Tuesday</td>
+						<td>11am - 10pm</td>
+					</tr>
+					<tr>
+						<td>Wednesday</td>
+						<td>11am - 10pm</td>
+					</tr>
+					<tr>
+						<td>Thursday</td>
+						<td>11am - 10pm</td>
+					</tr>
+					<tr>
+						<td>Friday</td>
+						<td>11am - 10pm</td>
+					</tr>
+					<tr class="closed">
+						<td>Saturday</td>
+						<td>Closed</td>
+					</tr>
+					<tr class="closed">
+						<td>Sunday</td>
+						<td>Closed</td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="reservation flex-column">
+				<p>
+					Reserve your table at Dewy Berry to enjoy an authentic Italian dining
+					experience without the wait. Booking is quick and easy—secure your
+					spot by clicking the button below.
+				</p>
+				<button class="primary-btn reservation-btn">Book a Reservation</button>
+			</div>
+  `;
+}
+
+export default generateHomepage;
