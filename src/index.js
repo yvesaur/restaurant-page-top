e2511c3df501271fd2736795bf25d98ec913dff2
yvesaur <yveschristianpcasio@gmail.com>
2025-10-02 01:58:33 +0800

    feat: Homepage styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import "./assets/styles/index.css";
+/* CSS Reset
+   ========================================================================== */
+import "./assets/styles/normalize.css";
+/* ========================================================================== */
+
+/* Custom Styles
+   ========================================================================== */
+import "./assets/styles/components/buttonStyles.css";
+import "./assets/styles/layoutStyles.css";
+/* ========================================================================== */
+import "./assets/styles/colorPalette.css";
+import "./index.css";
 
 console.log("Webpack is running");
